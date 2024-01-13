9de24ab343d7c83e3da4f956079d60a450e76d9c
Satya Pavan <102037431+SatyaPavan6656@users.noreply.github.com>
2024-01-13 15:24:14 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Insertion Sort for Singly Linked List/insertion-sort-for-singly-linked-list.java
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+class insertion
+{
+    Node head;  
+    Node tail;
+	public void addToTheLast(Node node) 
+	{
+	  if (head == null) 
+	  {
+	   head = node;
+	   tail = node;
+	  } 
+	  else 
+	  {
+	   tail.next = node;
+	   tail = node;
+	  }
+	}
+      void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	/* Drier program to test above functions */
+	public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			insertion llist = new insertion(); 
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+			
+        Solution ob = new Solution();
+		head = ob.insertionSort(llist.head);
+		llist.printList(head);
+		
+        t--;		
+        }
+    }}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    */
+class Solution
+{
+    public static Node insertionSort(Node head_ref)
+    {
+        //code here
+        Node dummy = new Node(-1);
+        Node temp = null;
+        Node prev = null;
+        
+        // Iterate through the original linked list
+        while (head_ref != null) {
+            // Reset temp and prev pointers for each iteration
+            temp = dummy;
+            prev = null;
+            
+            // Find the correct position in the sorted part of the list
+            while (temp != null && head_ref.data > temp.data) {
+                prev = temp;
+                temp = temp.next;
+            }
+            
+            // Insert the current node into the sorted part of the list
+            prev.next = head_ref;
+            head_ref = head_ref.next;
+            prev.next.next = temp;
+        }
+        
+        // Return the sorted linked list starting from the next of the dummy node
+        return dummy.next;
+    }
+}
